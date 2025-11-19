aac2350f9ea0b06ef8695627843445c803e64782
leonardo <leonardo@passbolt.com>
2025-11-19 12:05:23 +0000

    PB-46388 Fix PHPUnit 11 Deprecations

--- plugins/PassboltCe/MultiFactorAuthentication/tests/TestCase/Utility/MfaVerifiedTokenTest.php
+++ plugins/PassboltCe/MultiFactorAuthentication/tests/TestCase/Utility/MfaVerifiedTokenTest.php
@@ -89,8 +89,9 @@ class MfaVerifiedTokenTest extends MfaIntegrationTestCase
     {
         $uac = UserFactory::make()->user()->persistedUAC();
         $sessionId = uniqid();
-        $stubSessionIdentifier = $this->getMockForAbstractClass(AbstractSessionIdentificationService::class);
+        $stubSessionIdentifier = $this->createStub(AbstractSessionIdentificationService::class);
         $stubSessionIdentifier->method('getSessionIdentifier')->willReturn($sessionId);
+        $stubSessionIdentifier->method('checkAuthenticationToken')->willReturn(true);
         $token = MfaVerifiedToken::get($uac, MfaSettings::PROVIDER_TOTP, $sessionId);
         $success = MfaVerifiedToken::check($uac, $token, $stubSessionIdentifier, new ServerRequest());
         $this->assertTrue($success);
@@ -132,8 +133,9 @@ class MfaVerifiedTokenTest extends MfaIntegrationTestCase
         $uac = UserFactory::make()->user()->persistedUAC();
         $uac2 = UserFactory::make()->user()->persistedUAC();
         $sessionId = 'Foo';
-        $stubSessionIdentifier = $this->getMockForAbstractClass(AbstractSessionIdentificationService::class);
+        $stubSessionIdentifier = $this->createStub(AbstractSessionIdentificationService::class);
         $stubSessionIdentifier->method('getSessionIdentifier')->willReturn($sessionId);
+        $stubSessionIdentifier->method('checkAuthenticationToken')->willReturn(true);
         $token = MfaVerifiedToken::get($uac, MfaSettings::PROVIDER_TOTP, $sessionId);
         $this->assertTrue(MfaVerifiedToken::check($uac, $token, $stubSessionIdentifier, new ServerRequest()));
         $this->assertFalse(MfaVerifiedToken::check($uac2, $token));
@@ -191,8 +193,9 @@ class MfaVerifiedTokenTest extends MfaIntegrationTestCase
         // Token is associated with another user agent
         $uac = UserFactory::make()->user()->persistedUAC();
         $sessionId = 'Foo';
-        $stubSessionIdentifier = $this->getMockForAbstractClass(AbstractSessionIdentificationService::class);
+        $stubSessionIdentifier = $this->createStub(AbstractSessionIdentificationService::class);
         $stubSessionIdentifier->method('getSessionIdentifier')->willReturn($sessionId);
+        $stubSessionIdentifier->method('checkAuthenticationToken')->willReturn(true);
         $entityData = [
             'user_id' => $uac->getId(),
             'token' => UuidFactory::uuid(),
