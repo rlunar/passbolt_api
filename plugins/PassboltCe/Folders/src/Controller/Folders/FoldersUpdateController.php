088278d09a70268073da5b86bafbcf68991535af
Juan Pablo Ramirez <pabloelcolombiano@gmail.com>
2025-12-10 22:37:49 +0100

    PB-47521 - As a user I should not get a 500 if a resource is deleted or I do not have access to it

--- plugins/PassboltCe/Folders/src/Controller/Folders/FoldersUpdateController.php
+++ plugins/PassboltCe/Folders/src/Controller/Folders/FoldersUpdateController.php
@@ -67,7 +67,7 @@ class FoldersUpdateController extends AppController
             ],
         ];
         $options = $this->QueryString->get($whitelist);
-        $folder = $foldersUpdateService->foldersTable->findView($this->User->id(), $folder->id, $options)->first();
+        $folder = $foldersUpdateService->foldersTable->findView($this->User->id(), $folder->id, $options)->firstOrFail();
         $folder = FolderizableBehavior::unsetPersonalPropertyIfNull($folder->toArray());
         $folder = (new MetadataFoldersRenderService())->renderFolder($folder, $folderDto->isV5());
 
